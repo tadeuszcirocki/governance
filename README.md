0591fc30dd7c31e9b883a2d48845d2c3addbff37
tadeusz-cirocki <tadeusz.cirocki@widelab.co>
2022-07-07 05:02:55 +0200

    wokring with one test case

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
-# Basic Sample Hardhat Project
+# Governance
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, a sample script that deploys that contract, and an example of a task implementation, which simply lists the available accounts.
+Build a simple smart contract project in Hardhat (using Solidity
+0.8.13) that introduces governance through OpenZeppelin's
+Governor and ERC721Votes from 4.5.0 release (it can be
+anything -> for example: simple control over ERC20 in Treasury).
+Ensure to have unit tests written to showcase proposal creation
+and voting.
 
-Try running some of the following tasks:
-
-```shell
-npx hardhat accounts
-npx hardhat compile
-npx hardhat clean
-npx hardhat test
-npx hardhat node
-node scripts/sample-script.js
-npx hardhat help
-```
+Contracts:
+MyGovernor.sol - governance contract using OpenZeppelin's Governor
+MyNFT.sol - vote token using OpenZeppelin's ERC721Votes
+MyToken.sol - ERC20 that governor has power over
--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,87 @@
 const { expect } = require("chai");
+const { BigNumber } = require("ethers");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
+describe("Governance", function () {
+
   it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+    // ERC721 vote token deployment
+    const MyNft = await ethers.getContractFactory("MyNFT");
+    const myNft = await MyNft.deploy();
+    await myNft.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    // governor with MyNFT votes deployment
+    const MyGovernor = await ethers.getContractFactory("MyGovernor");
+    const myGovernor = await MyGovernor.deploy(myNft.address);
+    await myGovernor.deployed();
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    // ERC20 mintable by governor deployment
+    const MyToken = await ethers.getContractFactory("MyToken");
+    const myToken = await MyToken.deploy(myGovernor.address);
+    await myToken.deployed();
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    // supply = 100, addr1 has 1, addr2 - 2, addr3 - 3; quorum = 5%
+    const [deployer, addr1, addr2, addr3] = await ethers.getSigners();
+    await mintNftsToAddress(deployer.address, 96);
+    await mintNftsToAddress(addr1.address, 1);
+    await mintNftsToAddress(addr2.address, 2);
+    await mintNftsToAddress(addr3.address, 3);
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    // each user will be its own delegate
+    await myNft.connect(addr1).delegate(addr1.address);
+    await myNft.connect(addr2).delegate(addr2.address);
+    await myNft.connect(addr3).delegate(addr3.address);
+
+    // making a proposal of minting 1000 MyTokens to address
+    const receiverAddress = '0x29D7d1dd5B6f9C864d9db560D72a247c178aE86B';
+    const amount = BigNumber.from(1000).mul(1e18.toString());  // MTK has 18 decimals
+    const mintCalldata = myToken.interface.encodeFunctionData('mint', [receiverAddress, amount]);
+    const description = 'Proposal 1: Mint 1000 MTK to address';
+    const tx = await myGovernor.propose(
+      [myToken.address],
+      [0],
+      [mintCalldata],
+      description
+    );
+    const rc = await tx.wait();
+    const event = rc.events.find(event => event.event === 'ProposalCreated');
+    const proposalId = event.args.proposalId;
+    //1 block has to pass (set in MyGovernor)
+    await network.provider.send("hardhat_mine", ['0x1']);
+    
+    // voting; '0' - against, '1' - for, '2' - abstain (does not count for quorum)
+    await myGovernor.connect(addr1).castVote(proposalId, 1);
+    await myGovernor.connect(addr2).castVote(proposalId, 1);
+    await myGovernor.connect(addr3).castVote(proposalId, 1);
+
+    // 45818 blocks got mined = one week has passed on eth mainnet (period set in MyGovernor)
+    const oneWeekInBlocks = 45818;
+    const oneWeekInBlocksHex = '0x' + oneWeekInBlocks.toString(16)
+    await network.provider.send("hardhat_mine", [oneWeekInBlocksHex]);
+
+    // execution
+    const descriptionHash = ethers.utils.id(description);
+    await myGovernor.execute(
+      [myToken.address],
+      [0],
+      [mintCalldata],
+      descriptionHash
+    );
+
+    console.log(await myToken.balanceOf(receiverAddress));
+
+
+    // helper functions
+
+    async function mintNftsToAddress(address, amount) {
+      for (i = 0; i < amount; i++) {
+        await myNft.safeMint(address);
+      }
+    }
+
+
   });
+
+
+
 });
